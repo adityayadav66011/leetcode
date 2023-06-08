a7b93fd540c15a0da7dc21a15e8a27ec31ecd152
aditya yadav <50453640+adityayadav66011@users.noreply.github.com>
2023-06-08 06:25:10 +0530

    Time: 28 ms (46.22%), Space: 19.3 MB (14.9%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,18 +1,5 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        int lastnonzeroindex=0;
-        for(int i=0;i<nums.size();i++)
-        {
-            if(nums[i]!=0)
-            {
-                nums[lastnonzeroindex++]=nums[i];
-            }
-        }
-        for(int i=lastnonzeroindex;i<nums.size();i++)
-        {
-            nums[i]=0;
-        }
-        
-    }
+    stable_partition(nums.begin(), nums.end(), [](int num) { return num != 0; });        }
 };
