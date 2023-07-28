cb48c75db97847af116923ebe022506fe6bf5aba
aditya yadav <50453640+adityayadav66011@users.noreply.github.com>
2023-07-28 17:42:32 +0530

    Time: 3 ms (93.85%), Space: 14 MB (50.43%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
     bool search(vector<int>& nums, int target) {
-      int l = 0;
-        int r = nums.size() - 1;
-        
-        while(l <= r)
+    int n=nums.size();
+    int low=0;
+    int high=n-1;
+    while(low<=high)
+    {
+        int mid=low+(high-low)/2;
+        if(nums[mid]==target)
         {
-            int mid = l + (r-l) / 2;
-            if (nums[mid] == target)
-                return true;
-			// with duplicates we can have this contdition, just update left & right
-            if((nums[l] == nums[mid]) && (nums[r] == nums[mid]))
-            {
-                l++;
-                r--;
-            }
-			// first half
-			// first half is in order
-            else if(nums[l] <= nums[mid])
-            {
-				// target is in first  half
-                if((nums[l] <= target) && (nums[mid] > target))
-                    r = mid - 1;
-                else
-                    l = mid + 1;
-            }
-			// second half
-			// second half is order
-			// target is in second half
+            return true;
+        }
+        if((nums[low]==nums[mid]) && (nums[mid]==nums[high]))
+        {
+            low++,high--;
+        }
+        //left sorted
+        else if(nums[low]<=nums[mid])
+        {
+            if(nums[low]<=target && nums[mid]>target)
+                high=mid-1;
             else
-            {
-                if((nums[mid] < target) && (nums[r]>= target))
-                    l = mid + 1;
-                else
-                    r = mid - 1;
-            }
+                low=mid+1;
+        }
+        //right sorted
+        else
+        {
+            if(nums[mid]<target && target<=nums[high])
+                low=mid+1;
+            else
+                high=mid-1;
+        }
         }
         return false;
-    }
+    }    
 };
