5bdffeeb28289b6ff4a489458e812f40f5d3e95c
aditya yadav <50453640+adityayadav66011@users.noreply.github.com>
2023-07-29 22:13:57 +0530

    Time: 32 ms (99.65%), Space: 18.8 MB (77.64%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.cpp
@@ -4,18 +4,18 @@ int minEatingSpeed(std::vector<int>& piles, int h) {
     int left = 1;
     int right = *std::max_element(piles.begin(), piles.end());
 
-    while (left < right) {
+    while (left <= right) {
         int mid = left + (right - left) / 2;
 
         // Calculate the total hours required to eat all bananas at speed 'mid'
-        int totalHours = 0;
+        double totalHours = 0;
         for (int pile : piles) {
             totalHours += (pile + mid-1 ) / mid;
         }
 
         if (totalHours <= h) {
             // We can finish eating in 'h' hours, so we try to minimize 'k'
-            right = mid;
+            right = mid-1;
         } else {
             // We cannot finish in 'h' hours, so we need to increase 'k'
             left = mid + 1;
