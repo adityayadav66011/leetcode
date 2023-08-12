edbabf2ba6987fc9a689bc7feaade8e888a3ba31
aditya yadav <50453640+adityayadav66011@users.noreply.github.com>
2023-08-12 21:05:02 +0530

    Time: 509 ms (8.11%), Space: 327.6 MB (5.08%) - LeetHub

--- 0816-design-hashset/0816-design-hashset.cpp
+++ 0816-design-hashset/0816-design-hashset.cpp
@@ -1,22 +1,37 @@
 class MyHashSet {
 public:
-vector<int> m;
+vector<list<int>> m;
     int size;
     MyHashSet() {
     size=1e6+1;
     m.resize(size);    
     }
+    int hash(int key)
+    {
+        return key%size;
+    }
+    list<int> :: iterator search(int key)
+    {
+        int i=hash(key);
+        return find(m[i].begin(),m[i].end(),key);
+    }
     
     void add(int key) {
-    m[key]=1;    
+    if(contains(key))return;
+    int i=hash(key);
+    m[i].push_back(key);    
     }
     
     void remove(int key) {
-    m[key]=0;    
+        if(!contains(key))return;
+        int i=hash(key);
+    m[i].erase(search(key));   
     }
     
     bool contains(int key) {
-    return m[key];    
+    int i=hash(key);
+    if(search(key)!=m[i].end()) return true;
+    else return false;    
     }
 };
 
