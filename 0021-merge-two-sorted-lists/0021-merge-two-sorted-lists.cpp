3625442079a18a23c4f143007a99ba665064e6ed
aditya yadav <50453640+adityayadav66011@users.noreply.github.com>
2023-08-13 15:05:51 +0530

    Time: 5 ms (74.13%), Space: 14.7 MB (56.07%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,6 +10,29 @@
  */
 class Solution {
 public:
+ListNode* merge(ListNode*l1,ListNode*l2)
+{
+    if(l1==NULL)
+    return l2;
+    if(l2==NULL)
+    return l1;
+    if(l1->val<l2->val)
+    {
+        l1->next=merge(l1->next,l2);
+        return l1;
+    }
+    else
+    {
+        l2->next=merge(l1,l2->next);
+        return l2;
+    }
+}
+ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2)
+ {
+     return merge(list1,list2);
+ }
+};
+/*
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
     ListNode* current=new ListNode();
     ListNode* head=current;    
@@ -38,3 +61,4 @@ public:
         return head->next;
     }
 };
+*/
