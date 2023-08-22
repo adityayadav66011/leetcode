8f0f43b691dbd6690ef5a19c70e97263fec04ee2
aditya yadav <50453640+adityayadav66011@users.noreply.github.com>
2023-08-22 13:10:10 +0530

    Time: 20 ms (54.83%), Space: 6 MB (23.36%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.cpp
+++ 0326-power-of-three/0326-power-of-three.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-       if (n < 1) {
+        if(n==0){
             return false;
         }
-
-        while (n % 3 == 0) {
-            n /= 3;
+        else if(n==1){
+            return true;
         }
-
-        return n == 1;    
+        return n%3==0 && isPowerOfThree(n/3);  
     }
 };
