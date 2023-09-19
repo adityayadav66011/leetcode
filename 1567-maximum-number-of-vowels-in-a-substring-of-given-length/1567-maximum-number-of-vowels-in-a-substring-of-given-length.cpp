e996f6b8f1c7080d17d722bbf4b20a3ac9d795ad
aditya yadav <50453640+adityayadav66011@users.noreply.github.com>
2023-09-19 19:15:26 +0530

    Time: 20 ms (59.11%), Space: 10.4 MB (11.13%) - LeetHub

--- 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1567-maximum-number-of-vowels-in-a-substring-of-given-length/1567-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     int maxVowels(string s, int k) {
-        unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u'};
-        int count = 0;
-        for (int i = 0; i < k; i++) {
-            count += vowels.count(s[i]);
-        } 
-        int answer = count;
-        for (int i = k; i < s.length(); i++) {
-            count += vowels.count(s[i]) - vowels.count(s[i - k]);
-            answer = max(answer, count);
+        
+        // Store current count of vowels in currCount
+        // and maximum currCount occurred in ans
+        int currCount = 0;
+        int ans = 0;
+        
+        // An array to mark all the vowels' position to true
+        // for checking whether the char is vowel or not
+        bool vowels[26] = {false};
+        vowels[0] = vowels[4] = vowels[8] = vowels[14] = vowels[20] = true;
+        
+        // Sliding window implementation
+        for (int i=0; i < s.length(); i++) {
+            
+            // if any vowel is removed from left-> decrement
+            if (i >= k and vowels[s[i-k]-'a']) {
+                currCount -= 1;
+            }
+            
+            // if any vowel is inserted from right-> increment
+            if (vowels[s[i]-'a']) {
+                currCount += 1;
+            }
+            
+            // Store maximum occurence of currCount in ans
+            ans = max (ans, currCount);
         }
         
-        return answer;
+        return ans ;
     }
 };
